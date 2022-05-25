0497e27960a8a0a2ca2fee9fb3fd95a2f2d6de98
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-05-25 16:31:50 -0300

    Criando projeto

---
+++ src/main/java/com/castello/crm/castellocrmapi/CastellocrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.castello.crm.castellocrmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastellocrmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastellocrmApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/castello/crm/castellocrmapi/CastellocrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castello.crm.castellocrmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastellocrmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
